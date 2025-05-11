e7237c00da33e9be271fda3032ad034ce1d69b9c
= <=>
2025-05-11 13:43:16 +0200

    Angular 2025 - Bases

---
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject, signal } from '@angular/core';
+import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import type { Character } from '../../interfaces/character.interface';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
+
+@Component({
+  templateUrl: './dragonball-super-page.component.html',
+  selector: 'dragonball-super',
+  imports: [CharacterListComponent, CharacterAddComponent],
+})
+export class DragonballSuperPageComponent {
+  public dragonballService = inject(DragonballService);
+}
